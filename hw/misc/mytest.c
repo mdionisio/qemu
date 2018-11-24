c4bae3daeee1002eecfca3e49a468b8ff1fe6437
Michele Dionisio <michele.dionisio@gmail.com>
2018-11-24 13:17:28 +0100

    [UPG] cleanup

--- hw/misc/mytest.c
+++ hw/misc/mytest.c
@@ -15,7 +15,7 @@
 
 #define DPRINTF(fmt, args...) \
     do { \
-        fprintf(stderr, "[%s:%d]%s: " fmt , __FILE__, __LINE__, \
+        fprintf(stderr, "[%s:%d]%s: " fmt "\n", __FILE__, __LINE__, \
                                             __func__, ##args);  \
     } while (0)
 
@@ -30,6 +30,7 @@
 
 void mytest_method(MyTest *obj)
 {
+    DPRINTF("");
     MyTestClass *klass = MYTEST_GET_CLASS(obj);
     if (klass->method != NULL) {
         klass->method(obj);
@@ -44,13 +45,13 @@ static uint64_t mytest_iomem_read(void *opaque, hwaddr offset, unsigned size)
 
     if (offset % 4 != 0) {
         qemu_log_mask(LOG_GUEST_ERROR, "[%s]%s: only aligned accessed allowed offset 0x%"
-                      HWADDR_PRIx "\n", TYPE_MYTEST, __func__, offset);
+                      HWADDR_PRIx, TYPE_MYTEST, __func__, offset);
         return 0;
     }
 
     switch (offset) {
         case 0:
-            DPRINTF("read(offset=0x%" HWADDR_PRIx ", size=%u)\n", offset, size);
+            DPRINTF("read(offset=0x%" HWADDR_PRIx ", size=%u)", offset, size);
 
             return s->data1;
         default:
@@ -79,12 +80,12 @@ static void mytest_iomem_write(void *opaque, hwaddr offset,
             s->data1 = (uint32_t)value;
             
             if (s->data1 == 0) {
-                DPRINTF("reset IRQ\n");
+                DPRINTF("reset IRQ");
                 timer_del(&(s->timer));
                 qemu_set_irq(s->irq, 0);
             } else if (s->data1 == 1) {
                 s->data1 = 0xFFFFFFFF;
-                DPRINTF("raise postponed\n");
+                DPRINTF("raise postponed");
                 timer_mod(&(s->timer), qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL) + NANOSECONDS_PER_SECOND/2);
             } else {
                 s->data1 ++;
@@ -115,10 +116,9 @@ static void mytest_get(Object *obj, Visitor *v, const char *name,
 {
     MyTest *s = MYTEST(obj);
     int64_t value = s->data1;
+    DPRINTF("");
 
     visit_type_int(v, name, &value, errp);
-
-    DPRINTF("mytest get\n");
 }
 
 static void mytest_set(Object *obj, Visitor *v, const char *name,
@@ -127,17 +127,17 @@ static void mytest_set(Object *obj, Visitor *v, const char *name,
     MyTest *s = MYTEST(obj);
     Error *local_err = NULL;
     int64_t temp;
+    DPRINTF("");
 
     visit_type_int(v, name, &temp, &local_err);
 
     s->data1 = temp;
-    DPRINTF("mytest set\n");
 }
 
 
 static void mytest_init(Object *obj)
 {
-    DPRINTF("mytest_init\n");
+    DPRINTF("");
     // DeviceState *d = DEVICE(obj);
     SysBusDevice *sbd = SYS_BUS_DEVICE(obj);
     MyTest *s = MYTEST(obj);
@@ -163,32 +163,18 @@ static void mytest_init(Object *obj)
 
 static void mytest_finalize(Object *obj) 
 {
-    DPRINTF("mytest_finalize\n");
+    DPRINTF("");
     MyTest *s = MYTEST(obj);
     timer_deinit(&(s->timer));
-}
-
-static DeviceReset old_callback_reset = NULL;
-static void mytest_reset_at_boot(DeviceState *dev)
-{
-    DPRINTF("reset on boot\n");
-    MyTest *s = MYTEST(dev);
-    s->data1 = 0;
-
-    if (old_callback_reset != NULL) {
-        old_callback_reset(dev);
-    }
-
-    timer_del(&(s->timer));
 }
 
 static void mytest_timer_expired(void *dev)
 {
     MyTest *s = MYTEST(dev);
 
-    DPRINTF("mytest timer expired\n");
+    DPRINTF("");
 
-    DPRINTF("raise IRQ\n");
+    DPRINTF("raise IRQ");
     qemu_set_irq(s->irq, 1);
 
     timer_del(&(s->timer));
@@ -197,7 +183,7 @@ static void mytest_timer_expired(void *dev)
 static DeviceRealize old_callback_realize = NULL;
 static void mytest_realize(DeviceState *dev, Error **errp)
 {
-    DPRINTF("initialization\n");
+    DPRINTF("");
 
     if (old_callback_realize != NULL) {
         old_callback_realize(dev, errp);
@@ -216,10 +202,12 @@ static void mytest_static_init(ObjectClass *klass, void *data)
     DeviceClass       *dc = DEVICE_CLASS(klass);
     //SysBusDeviceClass *sdc = SYS_BUS_DEVICE_CLASS(klass);
     MyTestClass       *mdc = MYTEST_CLASS(klass);
+
+    DPRINTF("");
+
     mdc->method = NULL;  // initialize our virtual method
 
     // overwrite virtual method
-    old_callback_reset =  dc->reset; dc->reset = mytest_reset_at_boot;
     old_callback_realize = dc->realize; dc->realize = mytest_realize;
 
     dc->desc = "MyTest Device";
@@ -229,17 +217,24 @@ static void mytest_static_init(ObjectClass *klass, void *data)
 }
 
 static const TypeInfo mytest_info = {
-    .name          = TYPE_MYTEST,
-    .parent        = TYPE_SYS_BUS_DEVICE,
-    .instance_size = sizeof(MyTest),
-    .instance_init = mytest_init,
-    .class_size    = sizeof(MyTestClass),
-    .class_init    = mytest_static_init,
-    .instance_finalize = mytest_finalize,
+    .name          = TYPE_MYTEST,           // The name of the type
+    .parent        = TYPE_SYS_BUS_DEVICE,   // The name of the parent type
+    .instance_size = sizeof(MyTest),        // The size of the object
+    .instance_init = mytest_init,           // This function is called to initialize an object
+    .instance_post_init = NULL,             // This function is called after all @instance_init functions were called
+    .abstract      = FALSE,                 // to specify it it can be directly initialized or not
+    .class_size    = sizeof(MyTestClass),   // The size of the class object (is 0 means size of the parent)
+    .class_init    = mytest_static_init,    // Class Initialized. Called after parent class initialized
+    .class_base_init = NULL,                // Call after before .class_init after all base class initialization but on all base class
+    .instance_finalize = mytest_finalize,   // This function is called during object destruction. And before parent object destruction
+    .class_finalize = NULL,                 // This function is called during class destruction
+    .class_data     = NULL,                 // Data to pass to the @class_init, @class_base_init and @class_finalize functions.
+    .interfaces     = NULL,                 // NULL terminater list of interface (Multihineritance of interface)
 };
 
 static void mytest_register_type(void)
 {
+    DPRINTF("");
     type_register_static(&mytest_info);
 }
 type_init(mytest_register_type)
